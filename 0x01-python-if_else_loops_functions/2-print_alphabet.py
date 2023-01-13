c21e4cb1d6112efc9613093624818ebd68f09b4d
Dumisani <dendumi@gmail.com>
2023-01-13 05:19:43 -0800

    Print Alphabet

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-for i in ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q",        "r","s","t","u","v","w","x","y","z"]:
-    print(i, end='')
+for i in ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q",
+        "r","s","t","u","v","w","x","y","z"]:
+    print("{}".format(i), end='')
