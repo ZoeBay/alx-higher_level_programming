c0c2c2f19f11f2a8edf36015297d257437241080
Dennis Bayani <dendumi@gmail.com>
2023-01-19 16:43:24 +0000

    5-print_comb2

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,8 @@
-#/usr/bin/python3
+#!/usr/bin/python3
 for num in range(0, 10):
     for nm in range(0, 10):
         if nm < 9:
             print("{}""{}".format(num, nm), end=', ')
         if nm == 9:
-            print("{}""{}".format(num, nm), end='\n')
+            print("{}""{}".format(num, nm), end=' ')
             break
