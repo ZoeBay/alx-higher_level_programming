a3fe776f7309338adf89217e572a0f673c7d3a49
Dennis Bayani <dendumi@gmail.com>
2023-01-21 08:45:23 +0000

    8-uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for char in str:
+        if ord(char) >= 0 and ord(char) <= 90:
+            char = char
+        else:
+	    char = chr(ord(char) - 32)
+
+        print("{}".format(chr(ord(char))), end=' ')
+    print('\n')
