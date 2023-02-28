6d0cdd8b96510d94eb101ccb1aeef8112bac8822
Dennis Bayani <dendumi@gmail.com>
2023-02-28 16:29:09 +0000

    4th Commit

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 for i in reversed(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
-                  "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]):
+                  "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x",
+                  "y", "z"]):
     if ord(i) % 2 != 0:
         i = i.upper()
     print("{}".format(i), end='')
